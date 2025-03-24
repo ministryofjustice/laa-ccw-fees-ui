292adaf72eff7a99998ac2ef9ffa9359f84d49c8
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-03-24 13:29:27 +0000

    api call setup

--- src/utils/axiosSetup.js
+++ src/utils/axiosSetup.js
@@ -1,27 +1,44 @@
-import axios from "axios";
-import { create } from "middleware-axios";
+import axios from 'axios';
+import { create } from 'middleware-axios';
+import config from '../../config.js';
 
 /**
  * Axios middleware to attach Axios instance to request object.
  *
  * @param {object} req - The Express request object.
- * @param {object} res - The Express response object.
+ * @param {object} _res - The Express response object.
  * @param {Function} next - The next middleware function in the stack.
  */
-export const axiosMiddleware = (req, res, next) => {
-  const protocol = req.protocol;
-  const host = req.get("host");
+export const axiosMiddleware = (req, _res, next) => {
+
+  const protocol = config.API_PROTOCOL;
+  const host = config.API_HOST
   const baseURL = `${protocol}://${host}`;
 
-  // Create wrapped instance of axios to use normal axios instance
   req.axiosMiddleware = create({
-    axiosInstance: axios, // Pass in the axios instance
+    axiosInstance: axios,
     baseURL: baseURL,
-    timeout: 5000, // Set a timeout value if needed
+    timeout: 5000, 
     headers: {
-      "Content-Type": "application/json",
-      // You can add other default headers here if needed
+      'Content-Type': 'application/json',
     },
   });
+
+  req.axiosMiddleware.axiosInstance.interceptors.request.use((config) => {
+    console.log('API request made to ', config.method.toUpperCase(), config.url);
+    return config;
+  }, (error) => {
+    console.error('API request Error:', error);
+    return Promise.reject(error);
+  });
+
+  req.axiosMiddleware.axiosInstance.interceptors.response.use((response) => {
+    console.log('API response Status:', response.status);
+    return response;
+  }, (error) => {
+    console.error('API response Error:', error);
+    return Promise.reject(error);
+  });
+
   next();
 };
