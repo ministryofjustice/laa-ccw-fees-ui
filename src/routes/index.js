bba1abb2ba400ebabb85d7a3842b9932e313bb60
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-03-25 16:28:41 +0000

    BILL-62 - send data to service and display result

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,39 @@
 import express from "express";
 const router = express.Router();
 
-/* GET home page. */
 router.get("/", async (req, res) => {
   try {
-    const response = await req.axiosMiddleware.get("/");
-
-    res.render("main/index", { response });
-  } catch {
+    res.render("main/index", { csrfToken: req.csrfToken() });
+  } catch (ex) {
     res.render("main/error", {
       status: "An error occurred",
       error: "An error occurred :(.",
     });
   }
+});
+
+router.post("/", async (req, res) => {
+
+  try {
+    console.log('in post')
+    const response = await req.axiosMiddleware.post("/fees/" + req.body.fee);
+    const number = response.data
+    console.log(number)
+    req.session.result = number;
+    console.log("before redirect")
+    res.redirect("/result")
+  } catch {
+    res.render("main/error", {
+      status: "An error occurred",
+      error: "An error occurred posting the answer :(.",
+    });
+  }
+});
+
+router.get("/result", (req, res) => {
+  console.log("followed redirect")
+
+  res.render("main/result", {number: req.session.result })
 });
 
 export default router;
--- src/routes/index.test.js
+++ src/routes/index.test.js
@@ -5,15 +5,14 @@ import indexRouter from "../routes/index";
 
 describe("GET /", () => {
   let app;
-  const renderMock = jest.fn();
+  const csrfMock = jest.fn();
   app = express();
 
   beforeEach(() => {
+
     // Mock the middleware
     app.use((req, _res, next) => {
-      // Make sure it exists
-      req.axiosMiddleware = req.axiosMiddleware || {};
-      req.axiosMiddleware.get = renderMock;
+      req.csrfToken = csrfMock;
       next();
     });
     app.use("/", indexRouter);
@@ -22,23 +21,22 @@ describe("GET /", () => {
     nunjucksSetup(app);
   });
 
-  it("should render index page when api call successful", async () => {
-    renderMock.mockReturnValue({
-      status: 200,
-      data: { blah: "foo", bar: "boo" },
-    });
 
+  it("should render index page", async () => {
+
+    csrfMock.mockReturnValue('mocked-csrf-token');
     const response = await request(app)
       .get("/")
       .expect("Content-Type", /html/)
       .expect(200);
 
-    expect(response.text).toContain("foo");
+    expect(response.text).toContain("Enter a number");
   });
 
-  it("should render error page when api call throws error", async () => {
-    renderMock.mockImplementation(() => {
-      throw new Error("API connection issue");
+  it("should render error page if fails to load page", async () => {
+
+    csrfMock.mockImplementation(() => {
+      throw new Error("token problems");
     });
 
     const response = await request(app)
@@ -48,4 +46,123 @@ describe("GET /", () => {
 
     expect(response.text).toContain("An error occurred");
   });
+
+});
+
+describe("POST /", () => {
+  let app;
+  let mockSession = {};
+  const renderMock = jest.fn();
+  app = express();
+
+  beforeEach(() => {
+
+    mockSession = {};
+    renderMock.mockReset();
+
+    // Mock the middleware
+    app.use((req, _res, next) => {
+      // Make sure it exists
+      req.axiosMiddleware = req.axiosMiddleware || {};
+      req.axiosMiddleware.post = renderMock;
+      req.session = mockSession;
+
+      req.body = {
+        fee: 123
+      }
+
+      next();
+    });
+    app.use("/", indexRouter);
+
+    // Would be nice to mock the nunjucks rendering but not managed to figure that bit out
+    nunjucksSetup(app);
+  });
+
+
+  it("should redirect to result page if successful call service", async () => {
+
+    renderMock.mockReturnValue({
+      status: 200,
+      data: "236.00"
+    });
+
+    await request(app)
+      .post("/")
+      .expect(302)
+      .expect('Location', '/result');
+
+    // Save value so result page can load it
+    expect(mockSession.result).toEqual("236.00");
+
+    expect(renderMock).toHaveBeenCalledWith("/fees/123")
+  });
+
+  it("should error if api call fails", async () => {
+
+    renderMock.mockImplementation(() => {
+      throw new Error("API connection issue");
+    });
+
+    const response = await request(app)
+      .post("/")
+      .expect("Content-Type", /html/)
+      .expect(200);
+
+    expect(response.text).toContain("An error occurred");
+
+    expect(mockSession.result).toBeUndefined();
+
+    expect(renderMock).toHaveBeenCalledWith("/fees/123")
+  });
+
+  //TODO error if data missing
+
+});
+
+describe("GET /result", () => {
+  let app;
+  app = express();
+
+  beforeEach(() => {
+
+    // Mock the middleware
+    app.use((req, _res, next) => {
+      req.session = {
+        result: "246.00"
+      }
+      next();
+    });
+    app.use("/", indexRouter);
+
+    // Would be nice to mock the nunjucks rendering but not managed to figure that bit out
+    nunjucksSetup(app);
+  });
+
+
+  it("should render result page", async () => {
+
+    const response = await request(app)
+      .get("/result")
+      .expect("Content-Type", /html/)
+      .expect(200);
+
+    expect(response.text).toContain("You should have £246.00");
+  });
+
+    //TODO error if data missing
+  // it("should render error page if fails to load page", async () => {
+
+  //   csrfMock.mockImplementation(() => {
+  //     throw new Error("token problems");
+  //   });
+
+  //   const response = await request(app)
+  //     .get("/")
+  //     .expect("Content-Type", /html/)
+  //     .expect(200);
+
+  //   expect(response.text).toContain("An error occurred");
+  // });
+
 });
