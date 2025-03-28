bb6a156fa53e62f0c55ca7719bb02864e2720ce0
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-03-28 12:04:01 +0000

    LPF-71 - rename to reflect page in prototype

--- src/routes/index.js
+++ src/routes/index.js
@@ -29,7 +29,7 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   // Remove any old data. They have restarted
   req.session.data = {};
-  res.redirect("/law-category");
+  res.redirect(getUrl("claimStart"));
 });
 
 router.get(getUrl("claimStart"), showClaimStartPage);
