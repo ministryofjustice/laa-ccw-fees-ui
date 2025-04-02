08f0ae84c6150201866cb87f26d9246b68324b52
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-04-02 16:18:55 +0100

    BILL-78 - case stage service

---
+++ src/service/caseStageService.test.js
@@ -0,0 +1,12 @@
+import {
+  isValidCaseStage} from "./caseStageService";
+
+describe("isValidCaseStage", () => {
+  it("should return true if valid case stage", () => {
+    expect(isValidCaseStage("LVL1")).toEqual(true);
+  });
+
+  it("should return false if invalid case stage", () => {
+    expect(isValidCaseStage("LVL3")).toEqual(false);
+  });
+});
