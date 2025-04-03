a2c38be044efb16350c0a4a2ab5c97ce4464bd62
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-04-03 15:55:05 +0100

    BILL-91 - add fallback error page for csrf errors

--- src/controllers/errorController.test.js
+++ src/controllers/errorController.test.js
@@ -1,19 +1,17 @@
 import { showGenericErrorPage } from "./errorController";
 
 describe("showGenericErrorPage", () => {
-
-    let req = {};
-    let res = {
-        render: jest.fn(),
-      };
+  let req = {};
+  let res = {
+    render: jest.fn(),
+  };
 
   it("should render start page", () => {
     showGenericErrorPage(req, res);
 
     expect(res.render).toHaveBeenCalledWith("main/error", {
-        error: "An error occurred.",
-        status: "An error occurred",
-      });
+      error: "An error occurred.",
+      status: "An error occurred",
+    });
   });
-
 });
