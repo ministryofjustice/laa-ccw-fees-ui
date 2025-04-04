3872da0813d6ab034a2d548e1c5a2d31c7ad2b39
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-04-04 17:05:16 +0100

    BILL-89 - matter code 1 service to call API

--- src/service/matterCode1Service.js
+++ src/service/matterCode1Service.js
@@ -1,37 +1,42 @@
-//TODO will be api call in future
-
-const matterCode1s = [
-  {
-    id: "FAMA",
-    description: "Divorce/Judicial Seperation/Nullity",
-  },
-  {
-    id: "FAMB",
-    description: "Dissolution of Civil Partnership",
-  },
-  {
-    id: "FAMC",
-    description: "Domestic Abuse",
-  },
-  {
-    id: "FAMD",
-    description: "Private Law Children Only",
-  },
-];
-
 /**
  * Gets the valid matter code 1s
- * @returns {Array[]} - the valid matter codes
+ * @param {import('express').Request} req Express request object
+ * @returns {Promise<Array[]>} - the valid matter codes
+ * @async
  */
-export function getMatterCode1s() {
-  return matterCode1s;
+export async function getMatterCode1s(req) {
+  if (req.session.data.validMatterCode1s == null) {
+    req.session.data.validMatterCode1s = await getMatterCode1sFromService(
+      req.axiosMiddleware,
+      req.session.data.lawCategory,
+    );
+  }
+
+  return req.session.data.validMatterCode1s;
 }
 
 /**
  * Check an entered matter code 1 is valid
+ * @param {Array[Object]} validMatterCode1s - list of valid matter codes returned by the getMatterCode1s routine
  * @param {string} enteredMatterCode - matter code 1 user selected
  * @returns {boolean} - true if matter code 1 is valid, false otherwise
  */
-export function isValidMatterCode1(enteredMatterCode) {
-  return matterCode1s.some((matterCode) => matterCode.id === enteredMatterCode);
+export function isValidMatterCode1(validMatterCode1s, enteredMatterCode) {
+  console.log(validMatterCode1s);
+  return validMatterCode1s.some(
+    (matterCode) => matterCode.matterCode === enteredMatterCode,
+  );
+}
+
+/**
+ * Get the matter code 1s from the backend service
+ * @param {import('middleware-axios').AxiosInstanceWrapper} axios - axios middleware instance
+ * @param {string} lawCategory - law category to get details for
+ * @returns {Promise<Array<object>>} - response from api
+ * @async
+ */
+async function getMatterCode1sFromService(axios, lawCategory) {
+  const response = await axios.get("/matter-codes/" + lawCategory);
+
+  return response.data.matterCodes;
 }
