d35f01561bcb30e4298e860c61cf681cd6817eda
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-04-07 17:38:37 +0100

    BILL-90 - clean up case stage data

--- src/service/sessionDataService.js
+++ src/service/sessionDataService.js
@@ -12,6 +12,7 @@
  * Cached data:
  *  - validMatterCode1s
  *  - validMatterCode2s
+ *  - validCaseStages
  */
 import {
   URL_ClaimStart,
@@ -25,12 +26,13 @@ import {
  * @param {string} page - page we are on and so determines what data needs cleaning
  */
 export function cleanData(req, page) {
-  //If we have changed data that other values depend on, we need to mop up that old data
+  // If we have changed data that other values depend on, we need to mop up that old data
   // E.g. matterCode2 depends on matterCode1 so changing matterCode1 invalidates any matterCode2 data
   switch (page) {
     case URL_ClaimStart:
       req.session.data.validMatterCode1s = null;
       req.session.data.validMatterCode2s = null;
+      req.session.data.validCaseStages = null;
       req.session.data.matterCode1 = null;
       req.session.data.matterCode2 = null;
       req.session.data.caseStage = null;
@@ -39,12 +41,14 @@ export function cleanData(req, page) {
       break;
     case URL_MatterCode1:
       req.session.data.validMatterCode2s = null;
+      req.session.data.validCaseStages = null;
       req.session.data.matterCode2 = null;
       req.session.data.caseStage = null;
       req.session.data.londonRate = null;
       req.session.data.vatIndicator = null;
       break;
     case URL_MatterCode2:
+      req.session.data.validCaseStages = null;
       req.session.data.caseStage = null;
       req.session.data.londonRate = null;
       req.session.data.vatIndicator = null;
--- src/service/sessionDataService.test.js
+++ src/service/sessionDataService.test.js
@@ -1,7 +1,13 @@
 import {
+  URL_CaseStage,
   URL_ClaimStart,
+  URL_ErrorPage,
+  URL_LondonRate,
   URL_MatterCode1,
   URL_MatterCode2,
+  URL_Result,
+  URL_Start,
+  URL_VatIndicator,
 } from "../routes/navigator";
 import { cleanData, getSessionData } from "./sessionDataService";
 
@@ -21,6 +27,7 @@ describe("cleanData", () => {
     req.session.data.londonRate = "f";
     req.session.data.vatIndicator = "g";
     req.session.data.startDate = "h";
+    req.session.data.validCaseStages = "i";
 
     cleanData(req, URL_ClaimStart);
 
@@ -32,6 +39,7 @@ describe("cleanData", () => {
     expect(req.session.data.londonRate).toEqual(null);
     expect(req.session.data.vatIndicator).toEqual(null);
     expect(req.session.data.startDate).toEqual("h");
+    expect(req.session.data.validCaseStages).toEqual(null);
   });
 
   it("should clean data when given MatterCode1 page", () => {
@@ -43,6 +51,7 @@ describe("cleanData", () => {
     req.session.data.londonRate = "f";
     req.session.data.vatIndicator = "g";
     req.session.data.startDate = "h";
+    req.session.data.validCaseStages = "i";
 
     cleanData(req, URL_MatterCode1);
 
@@ -54,6 +63,8 @@ describe("cleanData", () => {
     expect(req.session.data.londonRate).toEqual(null);
     expect(req.session.data.vatIndicator).toEqual(null);
     expect(req.session.data.startDate).toEqual("h");
+    expect(req.session.data.validCaseStages).toEqual(null);
+
   });
 
   it("should clean data when given MatterCode2 page", () => {
@@ -65,6 +76,7 @@ describe("cleanData", () => {
     req.session.data.londonRate = "f";
     req.session.data.vatIndicator = "g";
     req.session.data.startDate = "h";
+    req.session.data.validCaseStages = "i";
 
     cleanData(req, URL_MatterCode2);
 
@@ -76,6 +88,33 @@ describe("cleanData", () => {
     expect(req.session.data.londonRate).toEqual(null);
     expect(req.session.data.vatIndicator).toEqual(null);
     expect(req.session.data.startDate).toEqual("h");
+    expect(req.session.data.validCaseStages).toEqual(null);
+
+  });
+  
+  it.each([URL_Start, URL_Result, URL_LondonRate, URL_ErrorPage, URL_CaseStage, URL_VatIndicator])("should not clean data if any other page", (urlToTest) => {
+    req.session.data.validMatterCode1s = "a";
+    req.session.data.validMatterCode2s = "b";
+    req.session.data.matterCode1 = "c";
+    req.session.data.matterCode2 = "d";
+    req.session.data.caseStage = "e";
+    req.session.data.londonRate = "f";
+    req.session.data.vatIndicator = "g";
+    req.session.data.startDate = "h";
+    req.session.data.validCaseStages = "i";
+
+    cleanData(req, urlToTest);
+
+    expect(req.session.data.validMatterCode1s).toEqual("a");
+    expect(req.session.data.validMatterCode2s).toEqual("b");
+    expect(req.session.data.matterCode1).toEqual("c");
+    expect(req.session.data.matterCode2).toEqual("d");
+    expect(req.session.data.caseStage).toEqual("e");
+    expect(req.session.data.londonRate).toEqual("f");
+    expect(req.session.data.vatIndicator).toEqual("g");
+    expect(req.session.data.startDate).toEqual("h");
+    expect(req.session.data.validCaseStages).toEqual("i");
+
   });
 });
 
