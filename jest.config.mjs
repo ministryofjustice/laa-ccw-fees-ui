55f839a900ae6485d636057d4b9b4da154ee11cc
Chris Cook <christopher.cook@digital.justice.gov.uk>
2025-04-09 16:53:11 +0100

    add code coverage threshold

--- jest.config.mjs
+++ jest.config.mjs
@@ -43,7 +43,14 @@ const config = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
